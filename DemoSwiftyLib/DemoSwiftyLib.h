916b5783c10be578dd334aebb89e8c01e7c5751a
Isha Bansal <isha.bansal1994@gmail.com>
2023-03-20 12:27:39 +0530

    initial setup

---
+++ DemoSwiftyLib/DemoSwiftyLib.h
@@ -0,0 +1,18 @@
+//
+//  DemoSwiftyLib.h
+//  DemoSwiftyLib
+//
+//  Created by Isha Personal on 20/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoSwiftyLib.
+FOUNDATION_EXPORT double DemoSwiftyLibVersionNumber;
+
+//! Project version string for DemoSwiftyLib.
+FOUNDATION_EXPORT const unsigned char DemoSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoSwiftyLib/PublicHeader.h>
+
+
